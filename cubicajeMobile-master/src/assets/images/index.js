3f100836cd0f4573612b69f763021df00bfe1cce
KevinIgnacio <kevinignacio.garrido@gmail.com>
2025-07-02 20:17:51 -0400

    Create index.js

---
+++ cubicajeMobile-master/src/assets/images/index.js
@@ -0,0 +1,16 @@
+const images = {
+    spaceDimension: require('./spaceDimension.png'),
+    productDimension: require('./productDimension.png'),
+    productInfo: require('./productInfo.png'),
+    spaceDimension2: require('./spaceDimension2.png'),
+    planning: require('./planning.png'),
+    calculating: require('./calculating.png'),
+    altura: require('./altura.png'),
+    anchura: require('./anchura.png'),
+    profundidad: require('./profundidad.png'),
+    firstOrientation: require('./first-orientation.png'),
+    secondOrientation: require('./second-orientation.png'),
+    thirdOrientation: require('./third-orientation.png')
+}
+
+export default images;
